5155ce4020a24c5ef8b05412b64a0f86ac56552a
ciray <h031137@gmail.com>
2026-01-30 10:47:15 +0000

    feat: refactor main function and add template validation for Args struct

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,22 @@ use models::WorkRecord;
 use models::WorkRecords;
 use models::{Format, Query};
 
+fn main() {
+    let args = Args::parse();
+    if let Err(e) = args.validate() {
+        e.exit();
+    }
+
+    let bytes = include_bytes!("resources/data.csv.gz");
+    let records = read(bytes);
+
+    records
+        .filter(&args.query)
+        .random(!args.no_random)
+        .take(if args.all { records.len() } else { args.number })
+        .print(&args.format, args.template.as_ref());
+}
+
 #[derive(Parser, Debug)]
 #[command(version, about)]
 pub struct Args {
@@ -51,6 +67,21 @@ pub struct Args {
     pub template: Option<String>,
 }
 
+fn template_validator(s: &str) -> Result<String, String> {
+    if s.contains("{author}")
+        || s.contains("{title}")
+        || s.contains("{text}")
+        || s.contains("{url}")
+    {
+        Ok(s.to_string())
+    } else {
+        Err(
+            "Template must contain at least one of the placeholders: {author}, {title}, {text}, {url}"
+                .to_string(),
+        )
+    }
+}
+
 impl Args {
     fn validate(&self) -> Result<(), clap::Error> {
         if self.template.is_some() {
@@ -67,23 +98,6 @@ impl Args {
     }
 }
 
-fn main() {
-    let args = Args::parse();
-    if let Err(e) = args.validate() {
-        e.exit();
-    }
-
-    let bytes = include_bytes!("resources/data.csv.gz");
-    let records = read(bytes);
-
-    let n = if args.all { records.len() } else { args.number };
-    records
-        .filter(&args.query)
-        .random(!args.no_random)
-        .take(n)
-        .print(&args.format, args.template.as_ref());
-}
-
 fn read(bytes: &[u8]) -> Vec<WorkRecord> {
     let mut decompressed = Vec::new();
     MultiGzDecoder::new(bytes)
@@ -95,19 +109,4 @@ fn read(bytes: &[u8]) -> Vec<WorkRecord> {
         .from_reader(&decompressed[..]);
 
     csv.deserialize().filter_map(Result::ok).collect()
-}
-
-fn template_validator(s: &str) -> Result<String, String> {
-    if s.contains("{author}")
-        || s.contains("{title}")
-        || s.contains("{text}")
-        || s.contains("{url}")
-    {
-        Ok(s.to_string())
-    } else {
-        Err(
-            "Template must contain at least one of the placeholders: {author}, {title}, {text}, {url}"
-                .to_string(),
-        )
-    }
 }
