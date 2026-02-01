e59d14ee6ff512b53b71f3f30114a3d0db149f63
ciray <h031137@gmail.com>
2026-02-01 07:15:05 +0000

    feat: add flate2 dependency and refactor CSV generation with compression

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,8 +1,9 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
+use flate2::{Compression, write::GzEncoder};
 use rayon::prelude::*;
-use std::fs;
-use std::path::Path;
-use std::path::PathBuf;
+use std::fs::{File, create_dir_all};
+use std::io::copy;
+use std::path::{Path, PathBuf};
 
 mod extractor;
 mod models;
@@ -11,41 +12,11 @@ use extractor::{extract_authors, extract_links, extract_text_from_zip, extract_w
 use models::WorkRecord;
 
 const INPUT_PATH: &str = "aozorabunko";
-const OUTPUT_PATH: &str = "src/resources/data.csv";
+const OUTPUT_CSV_PATH: &str = "src/resources/data.csv";
+const OUTPUT_GZIP_PATH: &str = "src/resources/data.csv.gz";
 
 fn main() -> Result<()> {
-    generate_csv()?;
-
-    Ok(())
-}
-
-fn generate_csv() -> Result<()> {
-    let records: Vec<WorkRecord> =
-        extract_authors(&PathBuf::from(INPUT_PATH).join("index_pages/person_all.html"))
-            .unwrap_or_default()
-            .into_par_iter()
-            .flat_map(|author| {
-                extract_works(&author)
-                    .unwrap_or_default()
-                    .into_par_iter()
-                    .flat_map(move |work| {
-                        let work_link = extract_links(Path::new(&work.page_path))?;
-                        let text = extract_text_from_zip(Path::new(&work_link.zip_path))
-                            .unwrap_or_default();
-
-                        Some(WorkRecord {
-                            author_id: author.id.clone(),
-                            author_name: author.name.clone(),
-                            work_id: work.id,
-                            work_title: work.title,
-                            html_link: work_link.html_link,
-                            text,
-                        })
-                    })
-            })
-            .collect::<Vec<WorkRecord>>();
-
-    let mut records = records;
+    let mut records = extract(INPUT_PATH);
     records.sort_by_key(|r| {
         (
             r.author_name.clone(),
@@ -55,19 +26,46 @@ fn generate_csv() -> Result<()> {
         )
     });
 
-    write_csv(&records, OUTPUT_PATH)?;
+    write_csv(&records, OUTPUT_CSV_PATH)?;
+    compress_csv(OUTPUT_CSV_PATH, OUTPUT_GZIP_PATH)?;
 
     Ok(())
 }
 
-fn write_csv(records: &[WorkRecord], output_path: &str) -> Result<()> {
+fn extract(aozorabunko: &str) -> Vec<WorkRecord> {
+    extract_authors(&PathBuf::from(aozorabunko).join("index_pages/person_all.html"))
+        .unwrap_or_default()
+        .into_par_iter()
+        .flat_map(|author| {
+            extract_works(&author)
+                .unwrap_or_default()
+                .into_par_iter()
+                .flat_map(move |work| {
+                    let work_link = extract_links(Path::new(&work.page_path))?;
+                    let text =
+                        extract_text_from_zip(Path::new(&work_link.zip_path)).unwrap_or_default();
+
+                    Some(WorkRecord {
+                        author_id: author.id.clone(),
+                        author_name: author.name.clone(),
+                        work_id: work.id,
+                        work_title: work.title,
+                        html_link: work_link.html_link,
+                        text,
+                    })
+                })
+        })
+        .collect::<Vec<WorkRecord>>()
+}
+
+fn write_csv(records: &Vec<WorkRecord>, output_path: &str) -> Result<()> {
     if let Some(parent) = Path::new(output_path).parent() {
-        fs::create_dir_all(parent)?;
+        create_dir_all(parent)?;
     }
-    let mut wtr = csv::Writer::from_path(output_path)?;
+    let mut writer = csv::Writer::from_path(output_path)?;
 
     for record in records {
-        wtr.write_record([
+        writer.write_record([
             &record.author_name,
             &record.work_title,
             &record.text,
@@ -75,6 +73,18 @@ fn write_csv(records: &[WorkRecord], output_path: &str) -> Result<()> {
         ])?;
     }
 
-    wtr.flush()?;
+    writer.flush()?;
+
+    Ok(())
+}
+
+fn compress_csv(input_path: &str, output_path: &str) -> Result<()> {
+    let mut input = File::open(input_path)?;
+    let output = File::create(output_path)?;
+    let mut encoder = GzEncoder::new(output, Compression::default());
+
+    copy(&mut input, &mut encoder)?;
+    encoder.finish()?;
+
     Ok(())
 }
